f56c3dec9c6108d056110da258f7ed5bb453d032
Noelia-Abalo <nabalo@stuller.com>
2024-06-15 09:41:12 +0200

    feat (AWS): Comment request parameters, trying to deploy

--- AWS/alien-attack/short-labs/lib/SL01-parameterStoreStack.ts
+++ AWS/alien-attack/short-labs/lib/SL01-parameterStoreStack.ts
@@ -79,13 +79,13 @@ export class parameterStoreStack extends Stack {
         path: '/',
         options: {
           cacheNamespace: 'Alien-Attack_CacheNameSpace',
-          cacheKeyParameters: [/*'X-Amz-Target',*/ 'Content-Type'],
+          // cacheKeyParameters: ['X-Amz-Target', 'Content-Type'],
           credentialsRole: ssmRole,
           passthroughBehavior: PassthroughBehavior.WHEN_NO_TEMPLATES,
-          requestParameters: {
-            // 'X-Amz-Target': "'AmazonSSM.GetParameter'",
-            'Content-Type': "'application/x-amz-json-1.1'"
-          },
+          // requestParameters: {
+          //   'X-Amz-Target': "'AmazonSSM.GetParameter'",
+          //   'Content-Type': "'application/x-amz-json-1.1'"
+          // },
           requestTemplates: {
             'application/json': '{ "Name": "/WS/Alien-Attack/Short-Lab-01/configuration" }'
           }
