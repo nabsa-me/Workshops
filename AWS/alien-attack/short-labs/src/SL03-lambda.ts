161fe39d4e905776deb8164140e1f17b29e6e37a
Noabsa <noelia.abalo@gmail.com>
2024-06-16 12:40:19 +0200

    feat (AWS): SL03 - Add lambda code

--- AWS/alien-attack/short-labs/src/SL03-lambda.ts
+++ AWS/alien-attack/short-labs/src/SL03-lambda.ts
@@ -1,9 +1,50 @@
-export const handler = async (event: any) => {
-  // The following line records the received event at the function's logs on CloudWatch Logs
+import { SSMClient, GetParameterCommand } from '@aws-sdk/client-ssm'
+import { Handler } from 'aws-cdk-lib/aws-lambda'
+const SSM = new SSMClient()
+
+export const handler: Handler = async (event) => {
+  // logging the received event
   console.log(event)
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify('Hello from Lambda!')
-  }
-  return response
+  console.log(event.type)
+  let responseFromSSM = null
+  let result = null
+
+  if (!event.Name)
+    // event does not have the proper format
+    result = {
+      statusCode: 400,
+      body: 'Invalid parameter'
+    }
+  else
+    try {
+      // parameter expected by SSM.getParameter
+      let parameter = {
+        Name: '/WS/Alien-Attack/' + event.Name + '/configuration'
+      }
+      const getParameterCommand = new GetParameterCommand(parameter)
+      responseFromSSM = await SSM.send(getParameterCommand)
+      console.log('SUCCESS')
+      console.log(responseFromSSM)
+      let value = JSON.parse(responseFromSSM.Parameter?.Value as any)
+      console.log(value)
+      result = {
+        statusCode: 200,
+        body: JSON.stringify(value)
+      }
+    } catch (err) {
+      console.log('ERROR')
+      console.log(err)
+      console.log(err.type)
+      if (err.StatusCode)
+        result = {
+          statusCode: err.StatusCode,
+          body: err.code
+        }
+      else
+        result = {
+          statusCode: 500,
+          body: err.code
+        }
+    }
+  return result
 }
