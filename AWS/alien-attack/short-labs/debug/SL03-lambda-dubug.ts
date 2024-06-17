e098e45ac352be6349465a9975e8881f35292bf6
Noabsa <noelia.abalo@gmail.com>
2024-06-17 20:03:49 +0200

    chore (AWS): Change event name for debugger

--- AWS/alien-attack/short-labs/debug/SL03-lambda-dubug.ts
+++ AWS/alien-attack/short-labs/debug/SL03-lambda-dubug.ts
@@ -1,4 +1,4 @@
 import { handler } from '../src/SL03-lambda'
 
-const event: Record<'Name', string> = { Name: 'string' }
+const event: Record<'Name', string> = { Name: 'Short-Lab-03' }
 handler(event)
