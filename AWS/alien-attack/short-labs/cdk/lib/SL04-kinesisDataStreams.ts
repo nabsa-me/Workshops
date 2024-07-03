9aa10047abdc6d0659e2d8c7a713cd08ae980109
Noabsa <noelia.abalo@gmail.com>
2024-07-03 20:08:40 +0200

    feat (AWS): SL04 - Create Kinesis role and add to Rest Api

--- AWS/alien-attack/short-labs/cdk/lib/SL04-kinesisDataStreams.ts
+++ AWS/alien-attack/short-labs/cdk/lib/SL04-kinesisDataStreams.ts
@@ -1,5 +1,6 @@
 import { App, Stack, StackProps, Tags } from 'aws-cdk-lib'
-import { EndpointType, LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway'
+import { AwsIntegration, EndpointType, LambdaIntegration, PassthroughBehavior, RestApi } from 'aws-cdk-lib/aws-apigateway'
+import { Effect, Policy, PolicyStatement, Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam'
 import * as kinesis from 'aws-cdk-lib/aws-kinesis'
 // import { Effect, Policy, PolicyStatement, Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam'
 import { Architecture, Runtime, StartingPosition } from 'aws-cdk-lib/aws-lambda'
@@ -32,40 +33,40 @@ export class kinesisDataStreamsStack extends Stack {
 
     //#endregion
 
-    //#region IAM ROLE
-    // const ssmRole = new Role(this, 'WS-AlienAttack-Lab01-Role', {
-    //   assumedBy: new ServicePrincipal('apigateway.amazonaws.com'),
-    //   roleName: 'WS-AlienAttack-Lab01-Role',
-    //   description: 'role that will be counsumed by the api'
-    // })
+    //#region KINESIS ROLE
+    const kinesisRole = new Role(this, `${baseIDresource}-Role`, {
+      assumedBy: new ServicePrincipal('kinesis.amazonaws.com'),
+      roleName: `${baseIDresource}-Lambda`,
+      description: 'role that will be counsumed by the api'
+    })
 
-    // ssmRole.addToPolicy(
-    //   new PolicyStatement({
-    //     effect: Effect.ALLOW,
-    //     resources: ['*'],
-    //     actions: [
-    //       'logs:CreateLogGroup',
-    //       'logs:CreateLogStream',
-    //       'logs:DescribeLogGroups',
-    //       'logs:DescribeLogStreams',
-    //       'logs:PutLogEvents',
-    //       'logs:GetLogEvents',
-    //       'logs:FilterLogEvents'
-    //     ]
-    //   })
-    // )
-    // ssmRole.attachInlinePolicy(
-    //   new Policy(this, 'WS-AlienAttack-Lab01-Policy', {
-    //     policyName: 'WS-AlienAttack-Lab01-Policy',
-    //     statements: [
-    //       new PolicyStatement({
-    //         effect: Effect.ALLOW,
-    //         actions: ['ssm:GetParameter'],
-    //         resources: [`${arnParameter}`]
-    //       })
-    //     ]
-    //   })
-    // )
+    kinesisRole.addToPolicy(
+      new PolicyStatement({
+        effect: Effect.ALLOW,
+        resources: ['*'],
+        actions: [
+          'logs:CreateLogGroup',
+          'logs:CreateLogStream',
+          'logs:DescribeLogGroups',
+          'logs:DescribeLogStreams',
+          'logs:PutLogEvents',
+          'logs:GetLogEvents',
+          'logs:FilterLogEvents'
+        ]
+      })
+    )
+    kinesisRole.attachInlinePolicy(
+      new Policy(this, `${baseIDresource}-Policy`, {
+        policyName: `${baseIDresource}-Policy`,
+        statements: [
+          new PolicyStatement({
+            effect: Effect.ALLOW,
+            actions: ['kinesis:PutRecord'],
+            resources: ['*']
+          })
+        ]
+      })
+    )
 
     //#endregion
 
@@ -92,7 +93,41 @@ export class kinesisDataStreamsStack extends Stack {
       endpointConfiguration: { types: [EndpointType.REGIONAL] }
     })
 
-    api.root.addResource('putorder').addMethod('POST', new LambdaIntegration(lambda))
+    api.root.addResource('putorder').addMethod('POST', new AwsIntegration({
+      service: 'kinesis',
+      region: this.region,
+      integrationHttpMethod: 'POST',
+      path: '/',
+      action:"PutRecord",
+      options: {
+        credentialsRole: kinesisRole,
+        passthroughBehavior: PassthroughBehavior.WHEN_NO_TEMPLATES,
+        
+//         requestParameters: {
+//           'integration.request.header.X-Amz-Target': "'AmazonSSM.GetParameter'",
+//           'integration.request.header.Content-Type': "'application/x-amz-json-1.1'"
+//         },
+//         cacheKeyParameters: ['integration.request.header.X-Amz-Target', 'integration.request.header.Content-Type'],
+//         requestTemplates: {
+//           'application/json': JSON.stringify({
+//             Name: "/WS/Alien-Attack/$input.params('lab')/configuration"
+//           })
+//         },
+//         integrationResponses: [
+//           {
+//             statusCode: '200',
+//             responseTemplates: {
+//               'application/json': `#set($inputRoot=$input.path('$'))
+// #if ($inputRoot.Parameter.Value && $inputRoot.Parameter.Value!="")
+// {
+// "data" : $input.path('$').Parameter.Value
+// }
+// #end`
+//             }
+//           }
+//         ]
+      }
+    }),)
 
     //#endregion
   }
