bb8d971c27a19b3f0f70042b3d092f09fdff441e
Noabsa <noelia.abalo@gmail.com>
2024-07-10 17:46:33 +0200

    feat (AWS): SL04 - Modify policies and lambda

--- AWS/alien-attack/short-labs/cdk/lib/SL04-kinesisDataStreams.ts
+++ AWS/alien-attack/short-labs/cdk/lib/SL04-kinesisDataStreams.ts
@@ -1,12 +1,11 @@
 import { App, Stack, StackProps, Tags } from 'aws-cdk-lib'
-import { AwsIntegration, EndpointType, LambdaIntegration, PassthroughBehavior, RestApi } from 'aws-cdk-lib/aws-apigateway'
-import { Effect, Policy, PolicyStatement, Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam'
-import * as kinesis from 'aws-cdk-lib/aws-kinesis'
-// import { Effect, Policy, PolicyStatement, Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam'
+import { AwsIntegration, EndpointType, PassthroughBehavior, RestApi } from 'aws-cdk-lib/aws-apigateway'
+import { AttributeType, TableV2 } from 'aws-cdk-lib/aws-dynamodb'
+import { Effect, PolicyStatement, Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam'
+import { Stream, StreamMode } from 'aws-cdk-lib/aws-kinesis'
 import { Architecture, Runtime, StartingPosition } from 'aws-cdk-lib/aws-lambda'
 import { KinesisEventSource } from 'aws-cdk-lib/aws-lambda-event-sources'
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs'
-// import { ParameterDataType, ParameterTier, StringParameter } from 'aws-cdk-lib/aws-ssm'
 import path from 'path'
 
 
@@ -19,31 +18,39 @@ export class kinesisDataStreamsStack extends Stack {
     Tags.of(this).add('Project', 'Workshops')
 
     //#endregion
+
     const baseIDresource = 'WS-AlienAttack-Lab04'
 
-    //#region STRING PARAMETER
-    // const stringParameter = new StringParameter(this, 'WS-AlienAttack-Lab01-StringParameter', {
-    //   stringValue: '{"url" : "https://www.amazon.com"}',
-    //   description: 'string parameter will be consumed by the api',
-    //   parameterName: '/WS/Alien-Attack/Short-Lab-01/configuration',
-    //   tier: ParameterTier.STANDARD,
-    //   dataType: ParameterDataType.TEXT
-    // })
-    // const arnParameter = stringParameter.parameterArn
+    //#region DYNAMO DB
+    const table = new TableV2(this, `${baseIDresource}-Table`, {
+      partitionKey: { name: 'transactionId', type: AttributeType.STRING },
+      tableName: `${baseIDresource}`
+    });
 
     //#endregion
 
-    //#region KINESIS ROLE
-    const kinesisRole = new Role(this, `${baseIDresource}-Role`, {
-      assumedBy: new ServicePrincipal('kinesis.amazonaws.com'),
-      roleName: `${baseIDresource}-Role`,
-      description: 'role that will be counsumed by the api'
+    //#region KINESIS STREAM
+    const kinesisStream=new Stream(this,`${baseIDresource}-Stream`,{
+      streamName:`${baseIDresource}-Stream`,
+      streamMode: StreamMode.ON_DEMAND
     })
 
-    kinesisRole.addToPolicy(
-      new PolicyStatement({
+    //#endregion
+
+    //#region IAM POLICIES AND ROLES
+    const dynamoPolicy=new PolicyStatement({
+      effect: Effect.ALLOW,
+      actions: ["dynamodb:BatchWriteItem"],
+      resources: [table.tableArn]
+    })
+    const kinesisPolicy=new PolicyStatement({
+          effect: Effect.ALLOW,
+          actions: ['kinesis:*'],
+          resources: [kinesisStream.streamArn]
+    })
+    const generalLogsPolicy=new PolicyStatement({
         effect: Effect.ALLOW,
-        resources: ['*'],
+        // we will add later the LAMBDA and the API resources to that POLICY
         actions: [
           'logs:CreateLogGroup',
           'logs:CreateLogStream',
@@ -53,19 +60,25 @@ export class kinesisDataStreamsStack extends Stack {
           'logs:GetLogEvents',
           'logs:FilterLogEvents'
         ]
-      })
-    )
-    const kinesisPolicy= new Policy(this, `${baseIDresource}-Policy`, {
-      policyName: `${baseIDresource}-Policy`,
-      statements: [
-        new PolicyStatement({
-          effect: Effect.ALLOW,
-          actions: ['kinesis:*'],
-          resources: ['*']
-        })
-      ]
     })
-    kinesisRole.attachInlinePolicy(kinesisPolicy)
+
+    const lambdaRole=new Role(this, `${baseIDresource}-LambdaRole`, {
+      assumedBy: new ServicePrincipal('lambda.amazonaws.com'),
+      roleName: `${baseIDresource}-LambdaRole`,
+      description: 'role for the lambda to manage kinesis data and write on dynamo db',
+    })
+    lambdaRole.addToPolicy(generalLogsPolicy)
+    lambdaRole.addToPolicy(dynamoPolicy)
+    lambdaRole.addToPolicy(kinesisPolicy)
+
+
+    const apiRole=new Role(this, `${baseIDresource}-ApiRole`, {
+      assumedBy: new ServicePrincipal('apigateway.amazonaws.com'),
+      roleName: `${baseIDresource}-ApiRole`,
+      description: 'role for the rest api to send kinesis events to lambda',
+    })
+    apiRole.addToPolicy(generalLogsPolicy)
+    apiRole.addToPolicy(kinesisPolicy)
 
     //#endregion
 
@@ -76,21 +89,24 @@ export class kinesisDataStreamsStack extends Stack {
       architecture: Architecture.ARM_64,
       handler: 'handler',
       entry: path.join(__dirname, '../../src/SL04-lambda.ts'),
+      role: lambdaRole,
+      environment:{ 'tableName': table.tableName }
     })
-    const stream = new kinesis.Stream(this,`${baseIDresource}-KinesisStream`,{streamName:`${baseIDresource}-KinesisStream`,shardCount:1})
 
-    lambda.addEventSource(new KinesisEventSource(stream, {
+    generalLogsPolicy.addResources(lambda.functionArn)
+
+    lambda.addEventSource(new KinesisEventSource(kinesisStream, {
       batchSize:10,
-      startingPosition:StartingPosition.TRIM_HORIZON
+      startingPosition:StartingPosition.LATEST
     }))
 
     //#endregion
+
     //#region REST API
     const api = new RestApi(this, `${baseIDresource}-API`, {
       restApiName: `${baseIDresource}-API`,
       description: 'api to fetch stored string parameters',
-      endpointConfiguration: { types: [EndpointType.REGIONAL] },
-      policy:kinesisPolicy.document
+      endpointConfiguration: { types: [EndpointType.REGIONAL] }
     })
 
     api.root.addResource('putorder').addMethod('POST', new AwsIntegration({
@@ -99,34 +115,26 @@ export class kinesisDataStreamsStack extends Stack {
       integrationHttpMethod: 'POST',
       action:"PutRecord",
       options: {
-        credentialsRole: kinesisRole,
+        credentialsRole: apiRole,
         passthroughBehavior: PassthroughBehavior.WHEN_NO_TEMPLATES,
-        
-//         requestParameters: {
-//           'integration.request.header.X-Amz-Target': "'Amazon.GetParameter'",
-//           'integration.request.header.Content-Type': "'application/x-amz-json-1.1'"
-//         },
-//         cacheKeyParameters: ['integration.request.header.X-Amz-Target', 'integration.request.header.Content-Type'],
-//         requestTemplates: {
-//           'application/json': JSON.stringify({
-//             Name: "/WS/Alien-Attack/$input.params('lab')/configuration"
-//           })
-//         },
-//         integrationResponses: [
-//           {
-//             statusCode: '200',
-//             responseTemplates: {
-//               'application/json': `#set($inputRoot=$input.path('$'))
-// #if ($inputRoot.Parameter.Value && $inputRoot.Parameter.Value!="")
-// {
-// "data" : $input.path('$').Parameter.Value
-// }
-// #end`
-//             }
-//           }
-//         ]
+        requestTemplates: {
+          'application/json':
+              `#set($inputPath = $input.path('$'))
+              {
+              "PartitionKey" : "$inputPath.User#$inputPath.Client#$inputPath.Order.Symbol#$inputPath.Order.Volume#$inputPath.Order.Price#$inputPath.Timestamp",
+              "Data" : "$util.base64Encode("$input.json('$')")",
+              "StreamName" : ${kinesisStream.streamName}
+              }`
+        },
+        integrationResponses: [
+          {
+            statusCode: '200'
+          }
+        ]
       }
     }),)
+    
+    generalLogsPolicy.addResources(api.arnForExecuteApi('POST', '/putorder'))
 
     //#endregion
   }
