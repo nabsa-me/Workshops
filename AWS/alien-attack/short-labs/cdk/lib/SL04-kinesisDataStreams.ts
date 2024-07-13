e0eb176af5cb4826a6ed6d8d56e92947a2175548
Noabsa <noelia.abalo@gmail.com>
2024-07-13 11:38:00 +0200

    feat (AWS): SL04 - Change response template and add esLint

--- AWS/alien-attack/short-labs/cdk/lib/SL04-kinesisDataStreams.ts
+++ AWS/alien-attack/short-labs/cdk/lib/SL04-kinesisDataStreams.ts
@@ -8,7 +8,6 @@ import { KinesisEventSource } from 'aws-cdk-lib/aws-lambda-event-sources'
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs'
 import path from 'path'
 
-
 export class kinesisDataStreamsStack extends Stack {
   constructor(scope: App, id: string, props: StackProps) {
     super(scope, id, props)
@@ -25,57 +24,56 @@ export class kinesisDataStreamsStack extends Stack {
     const table = new TableV2(this, `${baseIDresource}-Table`, {
       partitionKey: { name: 'transactionId', type: AttributeType.STRING },
       tableName: `${baseIDresource}`
-    });
+    })
 
     //#endregion
 
     //#region KINESIS STREAM
-    const kinesisStream=new Stream(this,`${baseIDresource}-Stream`,{
-      streamName:`${baseIDresource}-Stream`,
+    const kinesisStream = new Stream(this, `${baseIDresource}-Stream`, {
+      streamName: `${baseIDresource}-Stream`,
       streamMode: StreamMode.ON_DEMAND
     })
 
     //#endregion
 
     //#region IAM POLICIES AND ROLES
-    const dynamoPolicy=new PolicyStatement({
+    const dynamoPolicy = new PolicyStatement({
       effect: Effect.ALLOW,
-      actions: ["dynamodb:BatchWriteItem"],
+      actions: ['dynamodb:BatchWriteItem'],
       resources: [table.tableArn]
     })
-    const kinesisPolicy=new PolicyStatement({
-          effect: Effect.ALLOW,
-          actions: ['kinesis:*'],
-          resources: [kinesisStream.streamArn]
+    const kinesisPolicy = new PolicyStatement({
+      effect: Effect.ALLOW,
+      actions: ['kinesis:*'],
+      resources: [kinesisStream.streamArn]
     })
-    const generalLogsPolicy=new PolicyStatement({
-        effect: Effect.ALLOW,
-        // we will add later the LAMBDA and the API resources to that POLICY
-        actions: [
-          'logs:CreateLogGroup',
-          'logs:CreateLogStream',
-          'logs:DescribeLogGroups',
-          'logs:DescribeLogStreams',
-          'logs:PutLogEvents',
-          'logs:GetLogEvents',
-          'logs:FilterLogEvents'
-        ]
+    const generalLogsPolicy = new PolicyStatement({
+      effect: Effect.ALLOW,
+      // we will add later the LAMBDA and the API resources to that POLICY
+      actions: [
+        'logs:CreateLogGroup',
+        'logs:CreateLogStream',
+        'logs:DescribeLogGroups',
+        'logs:DescribeLogStreams',
+        'logs:PutLogEvents',
+        'logs:GetLogEvents',
+        'logs:FilterLogEvents'
+      ]
     })
 
-    const lambdaRole=new Role(this, `${baseIDresource}-LambdaRole`, {
+    const lambdaRole = new Role(this, `${baseIDresource}-LambdaRole`, {
       assumedBy: new ServicePrincipal('lambda.amazonaws.com'),
       roleName: `${baseIDresource}-LambdaRole`,
-      description: 'role for the lambda to manage kinesis data and write on dynamo db',
+      description: 'role for the lambda to manage kinesis data and write on dynamo db'
     })
     // lambdaRole.addToPolicy(generalLogsPolicy)
     lambdaRole.addToPolicy(dynamoPolicy)
     lambdaRole.addToPolicy(kinesisPolicy)
 
-
-    const apiRole=new Role(this, `${baseIDresource}-ApiRole`, {
+    const apiRole = new Role(this, `${baseIDresource}-ApiRole`, {
       assumedBy: new ServicePrincipal('apigateway.amazonaws.com'),
       roleName: `${baseIDresource}-ApiRole`,
-      description: 'role for the rest api to send kinesis events to lambda',
+      description: 'role for the rest api to send kinesis events to lambda'
     })
     // apiRole.addToPolicy(generalLogsPolicy)
     apiRole.addToPolicy(kinesisPolicy)
@@ -90,15 +88,17 @@ export class kinesisDataStreamsStack extends Stack {
       handler: 'handler',
       entry: path.join(__dirname, '../../src/SL04-lambda.ts'),
       role: lambdaRole,
-      environment:{ 'tableName': table.tableName }
+      environment: { tableName: table.tableName }
     })
 
     generalLogsPolicy.addResources(lambda.functionArn)
 
-    lambda.addEventSource(new KinesisEventSource(kinesisStream, {
-      batchSize:10,
-      startingPosition:StartingPosition.LATEST
-    }))
+    lambda.addEventSource(
+      new KinesisEventSource(kinesisStream, {
+        batchSize: 10,
+        startingPosition: StartingPosition.LATEST
+      })
+    )
 
     //#endregion
 
@@ -109,40 +109,36 @@ export class kinesisDataStreamsStack extends Stack {
       endpointConfiguration: { types: [EndpointType.REGIONAL] }
     })
 
-    api.root.addResource('putorder').addMethod('POST', new AwsIntegration({
-      service: 'kinesis',
-      region: this.region,
-      integrationHttpMethod: 'POST',
-      action:"PutRecord",
-      options: {
-        credentialsRole: apiRole,
-        passthroughBehavior: PassthroughBehavior.WHEN_NO_TEMPLATES,
-        requestTemplates: {
-          'application/json':
-              `#set($inputPath = $input.path('$'))
-              {
-              "PartitionKey" : "$inputPath.User#$inputPath.Client#$inputPath.Order.Symbol#$inputPath.Order.Volume#$inputPath.Order.Price#$inputPath.Timestamp",
-              "Data" : "$util.base64Encode("$input.json('$')")",
-              "StreamName" : ${kinesisStream.streamName}
-              }`
-        },
-        integrationResponses: [
-          {
-            statusCode: '200',
-            responseTemplates: {
-              'application/json':
-              `#set($inputPath = $input.path('$'))
-              {
-              "PartitionKey" : "$inputPath.User#$inputPath.Client#$inputPath.Order.Symbol#$inputPath.Order.Volume#$inputPath.Order.Price#$inputPath.Timestamp",
-              "Data" : "$util.base64Encode("$input.json('$')")",
-              "StreamName" : ${kinesisStream.streamName}
-              }`
+    api.root.addResource('putorder').addMethod(
+      'POST',
+      new AwsIntegration({
+        service: 'kinesis',
+        region: this.region,
+        integrationHttpMethod: 'POST',
+        action: 'PutRecord',
+        options: {
+          credentialsRole: apiRole,
+          passthroughBehavior: PassthroughBehavior.WHEN_NO_TEMPLATES,
+          requestTemplates: {
+            'application/json': `#set($inputPath = $input.path('$'))
+{
+"PartitionKey" : "$inputPath.User#$inputPath.Client#$inputPath.Order.Symbol#$inputPath.Order.Volume#$inputPath.Order.Price#$inputPath.Timestamp",
+"Data" : "$util.base64Encode("$input.json('$')")",
+"StreamName" : ${kinesisStream.streamName}
+}`
+          },
+          integrationResponses: [
+            {
+              statusCode: '200',
+              responseTemplates: {
+                'application/json': ''
+              }
             }
-          }
-        ]
-      }
-    }),)
-    
+          ]
+        }
+      })
+    )
+
     generalLogsPolicy.addResources(api.arnForExecuteApi('POST', '/putorder'))
 
     //#endregion
--- AWS/alien-attack/short-labs/src/SL04-lambda.ts
+++ AWS/alien-attack/short-labs/src/SL04-lambda.ts
@@ -1,77 +1,80 @@
-import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { BatchWriteCommand, DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb'
+import { BatchWriteCommand, DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb'
 
-const client = new DynamoDBClient();
-const documentClient = DynamoDBDocumentClient.from(client);
+const client = new DynamoDBClient()
+const documentClient = DynamoDBDocumentClient.from(client)
 const tableName = process.env.tableName
 
 export const handler = async (event: any) => {
-    // let's log the incoming event
-    let payloadAsString = JSON.stringify(event);
-    console.log(payloadAsString);
-    try{
-        let batch = [];
-        let putRequestBatches = [];
-        // event contains Records, which is an array with a certain structure
-        for(let i=0;i<event.Records.length;i++) {
-            //let's get the Record
-            let record = event.Records[i];
-            // let's decode the base64-encoded payload that was sent
-            let data = Buffer.from(record.kinesis.data,'base64').toString('utf-8');
-            // let's show the data
-            console.log(`Data received: ${data}`);
-            // let's show the timestamp in which it was received (approximately)
-            let receivedTst = new Date(record.kinesis.approximateArrivalTimestamp*1000);
-            console.log(`Received tst: ${receivedTst}`);
-            //-----
-            // The following part of the code deals with the DynamoDB batches
-            //-----
-            // 
-            // THE FOLLOWING TWO STATEMENTS WERE ADAPTED TO PREPARE
-            // THE RECORD FOR DYNAMODB
-            let dataForDynamoDB = JSON.parse(data);
-            dataForDynamoDB["TransactionId"] = 
-                dataForDynamoDB.User+"#"+
-                dataForDynamoDB.Client+"#"+
-                dataForDynamoDB.Order.Symbol+"#"+
-                dataForDynamoDB.Order.Volume+"#"+
-                dataForDynamoDB.Order.Price+"#"+
-                dataForDynamoDB.Timestamp;
-            // put data into the current batch
-            batch.push(
-                {
-                    "PutRequest" : { "Item" : dataForDynamoDB }
-                }
-            );
-            // Batches are limited to 25 items; so, we "close" a batch when we reach 25 items.
-            if (batch.length == 25 || i == event.Records.length-1) {
-                putRequestBatches.push(batch);
-                batch = [];
-            }
-        }
-        // Here we have in putRequestBatches an array of batches
-        for (let i=0; i< putRequestBatches.length; i++) {
-            let params = {
-                "RequestItems" : {
-                    [`${tableName}`] : putRequestBatches[i]
-                }
-            };
-            console.log(`Writing to dynamodb: ${JSON.stringify(params)}`);
-            let batchWriteCommand = new BatchWriteCommand(params);
-            let ddbResponse = await documentClient.send(batchWriteCommand);
-            console.log(ddbResponse);
-        }
-    } catch(e) {
-        //let's handle the errors, if any
-        console.log("Error:",e);
-        const response = {
-            statusCode : 200,
-            body : e
-        };
-        return response;
+  // let's log the incoming event
+  let payloadAsString = JSON.stringify(event)
+  console.log(payloadAsString)
+  try {
+    let batch = []
+    let putRequestBatches = []
+    // event contains Records, which is an array with a certain structure
+    for (let i = 0; i < event.Records.length; i++) {
+      //let's get the Record
+      let record = event.Records[i]
+      // let's decode the base64-encoded payload that was sent
+      let data = Buffer.from(record.kinesis.data, 'base64').toString('utf-8')
+      // let's show the data
+      console.log(`Data received: ${data}`)
+      // let's show the timestamp in which it was received (approximately)
+      let receivedTst = new Date(record.kinesis.approximateArrivalTimestamp * 1000)
+      console.log(`Received tst: ${receivedTst}`)
+      //-----
+      // The following part of the code deals with the DynamoDB batches
+      //-----
+      //
+      // THE FOLLOWING TWO STATEMENTS WERE ADAPTED TO PREPARE
+      // THE RECORD FOR DYNAMODB
+      let dataForDynamoDB = JSON.parse(data)
+      dataForDynamoDB['TransactionId'] =
+        dataForDynamoDB.User +
+        '#' +
+        dataForDynamoDB.Client +
+        '#' +
+        dataForDynamoDB.Order.Symbol +
+        '#' +
+        dataForDynamoDB.Order.Volume +
+        '#' +
+        dataForDynamoDB.Order.Price +
+        '#' +
+        dataForDynamoDB.Timestamp
+      // put data into the current batch
+      batch.push({
+        PutRequest: { Item: dataForDynamoDB }
+      })
+      // Batches are limited to 25 items; so, we "close" a batch when we reach 25 items.
+      if (batch.length == 25 || i == event.Records.length - 1) {
+        putRequestBatches.push(batch)
+        batch = []
+      }
     }
+    // Here we have in putRequestBatches an array of batches
+    for (let i = 0; i < putRequestBatches.length; i++) {
+      let params = {
+        RequestItems: {
+          [`${tableName}`]: putRequestBatches[i]
+        }
+      }
+      console.log(`Writing to dynamodb: ${JSON.stringify(params)}`)
+      let batchWriteCommand = new BatchWriteCommand(params)
+      let ddbResponse = await documentClient.send(batchWriteCommand)
+      console.log(ddbResponse)
+    }
+  } catch (e) {
+    //let's handle the errors, if any
+    console.log('Error:', e)
     const response = {
-        statusCode: 200
-    };
-    return response;
-};
+      statusCode: 200,
+      body: e
+    }
+    return response
+  }
+  const response = {
+    statusCode: 200
+  }
+  return response
+}
