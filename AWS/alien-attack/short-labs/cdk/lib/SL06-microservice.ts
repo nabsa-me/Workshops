5e9594b75dc7857eb1029a3a75f57c46148d7b94
Noabsa <noelia.abalo@gmail.com>
2024-08-14 19:35:44 +0200

    feat (AWS): SL06 - Changes on restApi lambda integration

--- AWS/alien-attack/short-labs/cdk/lib/SL06-microservice.ts
+++ AWS/alien-attack/short-labs/cdk/lib/SL06-microservice.ts
@@ -99,76 +99,79 @@ export class microserviceStack extends Stack {
 
     usagePlan.addApiKey(apiKey)
 
-    // api.root.addResource('topxstatistics').addMethod(
-    //   'GET',
-    //   new LambdaIntegration(lambda, {
-    //     requestParameters: {
-    //       'method.request.querystring.sessionId': "'true'",
-    //       'integration.request.querystring.sessionId': "'method.request.querystring.sessionId'"
-    //     },
-    //     passthroughBehavior: PassthroughBehavior.WHEN_NO_TEMPLATES,
-    //     requestTemplates: {
-    //       'application/json': `#set($allParams = $input.params())
-    //     {
-    //     "body-json" : $input.json('$'),
-    //     "params" : {
-    //     #foreach($type in $allParams.keySet())
-    //         #set($params = $allParams.get($type))
-    //     "$type" : {
-    //         #foreach($paramName in $params.keySet())
-    //         "$paramName" : "$util.escapeJavaScript($params.get($paramName))"
-    //             #if($foreach.hasNext),#end
-    //         #end
-    //     }
-    //         #if($foreach.hasNext),#end
-    //     #end
-    //     },
-    //     "stage-variables" : {
-    //     #foreach($key in $stageVariables.keySet())
-    //     "$key" : "$util.escapeJavaScript($stageVariables.get($key))"
-    //         #if($foreach.hasNext),#end
-    //     #end
-    //     },
-    //     "context" : {
-    //         "account-id" : "$context.identity.accountId",
-    //         "api-id" : "$context.apiId",
-    //         "api-key" : "$context.identity.apiKey",
-    //         "authorizer-principal-id" : "$context.authorizer.principalId",
-    //         "caller" : "$context.identity.caller",
-    //         "cognito-authentication-provider" : "$context.identity.cognitoAuthenticationProvider",
-    //         "cognito-authentication-type" : "$context.identity.cognitoAuthenticationType",
-    //         "cognito-identity-id" : "$context.identity.cognitoIdentityId",
-    //         "cognito-identity-pool-id" : "$context.identity.cognitoIdentityPoolId",
-    //         "http-method" : "$context.httpMethod",
-    //         "stage" : "$context.stage",
-    //         "source-ip" : "$context.identity.sourceIp",
-    //         "user" : "$context.identity.user",
-    //         "user-agent" : "$context.identity.userAgent",
-    //         "user-arn" : "$context.identity.userArn",
-    //         "request-id" : "$context.requestId",
-    //         "resource-id" : "$context.resourceId",
-    //         "resource-path" : "$context.resourcePath"
-    //         }
-    //     }`
-    //     },
-    //     integrationResponses: [
-    //       {
-    //         statusCode: '200',
-    //         responseTemplates: {
-    //           'application/json': `#set($inputRoot = $input.path('$'))
-    //         ## The next line changes the HTTP response code with the one provided by the Lambda Function
-    //         #set($context.responseOverride.status = $inputRoot.statusCode)
-    //         ## Decoding base64 (this could have been left to the application)
-    //         #if( $inputRoot.isBase64Encoded == true )
-    //         $util.base64Decode($inputRoot.body)
-    //         #else
-    //         $inputRoot.body
-    //         #end`
-    //         }
-    //       }
-    //     ]
-    //   })
-    // )
+    api.root.addResource('topxstatistics').addMethod(
+      'GET',
+      new LambdaIntegration(lambda, {
+        requestParameters: {
+          'method.request.querystring.sessionId': 'sessionId',
+          'integration.request.querystring.sessionId': 'method.request.querystring.sessionId'
+        },
+        passthroughBehavior: PassthroughBehavior.WHEN_NO_TEMPLATES,
+        requestTemplates: {
+          'application/json': `##  See https://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-mapping-template-reference.html
+##  This template will pass through all parameters including path, querystring, header, stage variables, and context through to the integration endpoint via the body/payload
+#set($allParams = $input.params())
+{
+"body-json" : $input.json('$'),
+"params" : {
+#foreach($type in $allParams.keySet())
+    #set($params = $allParams.get($type))
+"$type" : {
+    #foreach($paramName in $params.keySet())
+    "$paramName" : "$util.escapeJavaScript($params.get($paramName))"
+        #if($foreach.hasNext),#end
+    #end
+}
+    #if($foreach.hasNext),#end
+#end
+},
+"stage-variables" : {
+#foreach($key in $stageVariables.keySet())
+"$key" : "$util.escapeJavaScript($stageVariables.get($key))"
+    #if($foreach.hasNext),#end
+#end
+},
+"context" : {
+    "account-id" : "$context.identity.accountId",
+    "api-id" : "$context.apiId",
+    "api-key" : "$context.identity.apiKey",
+    "authorizer-principal-id" : "$context.authorizer.principalId",
+    "caller" : "$context.identity.caller",
+    "cognito-authentication-provider" : "$context.identity.cognitoAuthenticationProvider",
+    "cognito-authentication-type" : "$context.identity.cognitoAuthenticationType",
+    "cognito-identity-id" : "$context.identity.cognitoIdentityId",
+    "cognito-identity-pool-id" : "$context.identity.cognitoIdentityPoolId",
+    "http-method" : "$context.httpMethod",
+    "stage" : "$context.stage",
+    "source-ip" : "$context.identity.sourceIp",
+    "user" : "$context.identity.user",
+    "user-agent" : "$context.identity.userAgent",
+    "user-arn" : "$context.identity.userArn",
+    "request-id" : "$context.requestId",
+    "resource-id" : "$context.resourceId",
+    "resource-path" : "$context.resourcePath"
+    }
+}`
+        },
+        integrationResponses: [
+          {
+            statusCode: '200',
+            responseTemplates: {
+              'application/json': `#set($inputRoot = $input.path('$'))
+## The next line changes the HTTP response code with the one provided by the Lambda Function
+#set($context.responseOverride.status = $inputRoot.statusCode)
+## Decoding base64 (this could have been left to the application)
+#if( $inputRoot.isBase64Encoded == true )
+$util.base64Decode($inputRoot.body)
+#else
+$inputRoot.body
+#end`
+            }
+          }
+        ]
+      }),
+      { requestParameters: { 'method.request.querystring.sessionId': true } }
+    )
 
     //#endregion
   }
