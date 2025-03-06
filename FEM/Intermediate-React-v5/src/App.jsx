de74ab751931b1d24cc4912f70ce9414ceeddb20
Noabsa <noelia.abalo@gmail.com>
2025-03-06 17:04:42 +0100

    feat (FEM): 🎥 Special Case React Tools

--- FEM/Intermediate-React-v5/src/App.jsx
+++ FEM/Intermediate-React-v5/src/App.jsx
@@ -3,6 +3,8 @@ import SearchParams from './SearchParams'
 import { Link, Routes, Route, BrowserRouter } from 'react-router-dom'
 import Details from './Details'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import AdoptedPetContext from './AdoptedPetContext'
+import { useState } from 'react'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -14,16 +16,20 @@ const queryClient = new QueryClient({
 })
 
 const App = () => {
+  const adoptedPetHook = useState(null)
+
   return (
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <header>
-          <Link to='/'>Adopt Me!</Link>
-        </header>
-        <Routes>
-          <Route path='/details/:id' element={<Details />} />
-          <Route path='/' element={<SearchParams />} />
-        </Routes>
+        <AdoptedPetContext.Provider value={adoptedPetHook}>
+          <header>
+            <Link to='/'>Adopt Me!</Link>
+          </header>
+          <Routes>
+            <Route path='/details/:id' element={<Details />} />
+            <Route path='/' element={<SearchParams />} />
+          </Routes>
+        </AdoptedPetContext.Provider>
       </QueryClientProvider>
     </BrowserRouter>
   )
--- FEM/Intermediate-React-v5/src/SearchParams.jsx
+++ FEM/Intermediate-React-v5/src/SearchParams.jsx
@@ -1,13 +1,18 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import { useQuery } from '@tanstack/react-query'
-import fetchSearch from './fetchSearch'
-import useBreedList from './useBreedList'
 import Results from './Results'
-
+import AdoptedPetContext from './AdoptedPetContext'
+import useBreedList from './useBreedList'
+import fetchSearch from './fetchSearch'
 const ANIMALS = ['bird', 'cat', 'dog', 'rabbit', 'reptile']
 
 const SearchParams = () => {
-  const [requestParams, setRequestParams] = useState({ location: '', animal: '', breed: '' })
+  const [requestParams, setRequestParams] = useState({
+    location: '',
+    animal: '',
+    breed: ''
+  })
+  const [adoptedPet] = useContext(AdoptedPetContext)
   const [animal, setAnimal] = useState('')
   const [breeds] = useBreedList(animal)
 
@@ -17,9 +22,9 @@ const SearchParams = () => {
   return (
     <div className='search-params'>
       <form
-        onSubmit={(event) => {
-          event.preventDefault()
-          const formData = new FormData(event.target)
+        onSubmit={(e) => {
+          e.preventDefault()
+          const formData = new FormData(e.target)
           const obj = {
             animal: formData.get('animal') ?? '',
             breed: formData.get('breed') ?? '',
@@ -28,34 +33,49 @@ const SearchParams = () => {
           setRequestParams(obj)
         }}
       >
+        {adoptedPet ? (
+          <div className='pet image-container'>
+            <img src={adoptedPet.images[0]} alt={adoptedPet.name} />
+          </div>
+        ) : null}
         <label htmlFor='location'>
           Location
-          <input name='location' id='location' placeholder='location' />
+          <input id='location' name='location' placeholder='Location' />
         </label>
+
         <label htmlFor='animal'>
           Animal
           <select
             id='animal'
-            value={animal}
-            onChange={(event) => {
-              setAnimal(event.target.value)
+            name='animal'
+            onChange={(e) => {
+              setAnimal(e.target.value)
+            }}
+            onBlur={(e) => {
+              setAnimal(e.target.value)
             }}
           >
             <option />
             {ANIMALS.map((animal) => (
-              <option key={animal}>{animal}</option>
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
             ))}
           </select>
         </label>
+
         <label htmlFor='breed'>
           Breed
-          <select id='breed' disabled={breeds.length === 0} name='breed'>
+          <select disabled={!breeds.length} id='breed' name='breed'>
             <option />
             {breeds.map((breed) => (
-              <option key={breed}>{breed}</option>
+              <option key={breed} value={breed}>
+                {breed}
+              </option>
             ))}
           </select>
         </label>
+
         <button>Submit</button>
       </form>
       <Results pets={pets} />
