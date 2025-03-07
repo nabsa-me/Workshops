83325c0a54ce7e18f27924964a14e74087ae7794
Noabsa <noelia.abalo@gmail.com>
2025-03-07 20:59:38 +0100

    feat (FEM): 🎥 Advanced React Performance

--- FEM/Intermediate-React-v5/src/App.jsx
+++ FEM/Intermediate-React-v5/src/App.jsx
@@ -1,16 +1,17 @@
-import { createRoot } from 'react-dom/client'
-import SearchParams from './SearchParams'
-import { Link, Routes, Route, BrowserRouter } from 'react-router-dom'
-import Details from './Details'
+import { Link, Routes, Route } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import AdoptedPetContext from './AdoptedPetContext'
-import { useState } from 'react'
+import { useState, lazy, Suspense } from 'react'
+
+const Details = lazy(() => import('./Details'))
+const SearchParams = lazy(() => import('./SearchParams'))
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       staleTime: Infinity,
-      cacheTime: Infinity
+      cacheTime: Infinity,
+      suspense: true
     }
   }
 })
@@ -20,8 +21,14 @@ const App = () => {
 
   return (
     <div className='p-0 m-0' style={{ background: 'url(https://pets-images.dev-apis.com/pets/wallpaperA.jpg)' }}>
-      <BrowserRouter>
-        <QueryClientProvider client={queryClient}>
+      <QueryClientProvider client={queryClient}>
+        <Suspense
+          fallback={
+            <div className='loading-pane'>
+              <h2 className='loader'>🌀</h2>
+            </div>
+          }
+        >
           <AdoptedPetContext.Provider value={adoptedPetHook}>
             <header className='w-full mb-10 text-center p-7 bg-gradient-to-b from-yellow-400 via-orange-500 to bg-red-500'>
               <Link className='text-6xl text-white hover:text-gray-200' to='/'>
@@ -33,12 +40,10 @@ const App = () => {
               <Route path='/' element={<SearchParams />} />
             </Routes>
           </AdoptedPetContext.Provider>
-        </QueryClientProvider>
-      </BrowserRouter>
+        </Suspense>
+      </QueryClientProvider>
     </div>
   )
 }
 
-const container = document.getElementById('root')
-const root = createRoot(container)
-root.render(<App />)
+export default App
--- FEM/Intermediate-React-v5/src/Details.jsx
+++ FEM/Intermediate-React-v5/src/Details.jsx
@@ -1,11 +1,12 @@
 import { useNavigate, useParams } from 'react-router-dom'
 import { useQuery } from '@tanstack/react-query'
-import { useContext, useState } from 'react'
+import { lazy, useContext, useState } from 'react'
 import AdoptedPetContext from './AdoptedPetContext'
-import Modal from './Modal'
 import ErrorBoundary from './ErrorBoundary'
 import fetchPet from './fetchPet'
 import Carousel from './Carousel'
+
+const Modal = lazy(() => import('./Modal'))
 
 const Details = () => {
   const { id } = useParams()
