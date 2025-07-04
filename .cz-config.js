be5e1286e5a273bb564963d8925d70b3ec68a882
Noabsa <noelia.abalo@gmail.com>
2025-07-04 20:02:25 +0200

    🔧 chore(ci): add commits automatization

---
+++ .cz-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  types: [
+    { value: '✨ feat', name: '✨ feat: New feature or change to an existing one' },
+    { value: '🐛 fix', name: '🐛 fix: Fixing a bug' },
+    { value: '📝 docs', name: '📝 docs: Add, delete or update documentation and README' },
+    { value: '💄 style', name: '💄 style: Styles or formatting changes' },
+    { value: '♻️ refactor', name: '♻️ refactor: Clean or polish working pieces of code' },
+    { value: '🔧 chore', name: '🔧 chore: Updating build tools and packages' },
+    { value: '✅ test', name: '✅ test: Adding or modifiying tests' },
+    { value: '⚡ perf', name: '⚡ perf: Performance improvements' },
+    { value: '🛡️ security', name: '🛡️ security: Improve security facts as login, tokens, cifer, etc.' },
+  ],
+  scopes: ['katas', 'fem', 'tutorial', 'tests','ci'],
+  allowCustomScopes: true,
+  subjectLimit: 100
+};
