3fe6ac4421a79b36e907a076675deccb51ee5362
Noabsa <noelia.abalo@gmail.com>
2025-08-18 21:34:33 +0200

    🔧 chore(app): catch up with DnD app

--- DnD/frontend/src/components/common/buttons.tsx
+++ DnD/frontend/src/components/common/buttons.tsx
@@ -6,7 +6,7 @@ export const CTAButton = styled((props: ButtonProps) => <Button {...props} />)((
   fontFamily: "'Cinzel Decorative', serif",
   fontWeight: 700,
   borderColor: theme.palette.negative[300],
-  boxShadow: `0 0 35px ${theme.palette.secondary[300]}, 0 0 8px ${theme.palette.negative[300]}`,
+  boxShadow: `-5px 5px 45px -5px ${theme.palette.secondary[300]}, 5px -5px 25px -10px ${theme.palette.negative[300]}`,
   textShadow: `2px 2px 2px ${theme.palette.base[0]}`,
   position: 'relative',
   height: '2.8rem',
@@ -38,7 +38,7 @@ export const CTAButton = styled((props: ButtonProps) => <Button {...props} />)((
     zIndex: -1
   },
   '&:hover': {
-    boxShadow: `0 0 30px ${theme.palette.secondary[300]}, 0 0 15px ${theme.palette.negative[200]}`,
+    boxShadow: `0 0 30px -1px ${theme.palette.secondary[300]}, 0 0 15px -5px ${theme.palette.negative[300]}`,
     borderColor: theme.palette.negative[200],
     color: colors.white,
     '&::after': {
@@ -47,7 +47,7 @@ export const CTAButton = styled((props: ButtonProps) => <Button {...props} />)((
     }
   },
   '&:active': {
-    boxShadow: `0 0 35px ${theme.palette.secondary[300]}, 0 0 18px ${theme.palette.negative[200]}`,
+    boxShadow: `-2px 2px 35px -1px ${theme.palette.secondary[300]}, 2px -2px 18px -5px ${theme.palette.negative[200]}`,
     borderColor: theme.palette.negative[100],
     '&::after': {
       opacity: 0.4,
