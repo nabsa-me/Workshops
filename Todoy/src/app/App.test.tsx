69141424a48b4405dd931e2a0734102eea768d78
Noabsa <noelia.abalo@gmail.com>
2025-12-26 09:58:24 +0100

    🟦♻️ refactor(app): change file structure for better scalability

--- Todoy/src/app/App.test.tsx
+++ Todoy/src/app/App.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react'
-import App from '../src/App'
+import App from './App'
 
 describe('App component', () => {
   it('show welcome message', () => {
