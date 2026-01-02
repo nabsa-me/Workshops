1d89541eaf8d66b4cdfeaf13cd9fc197ebc4fd49
Noabsa <noelia.abalo@gmail.com>
2026-01-02 16:05:34 +0100

    ✅ test(app): add App tests

--- Todoy/src/app/App.test.tsx
+++ Todoy/src/app/App.test.tsx
@@ -1,11 +1,25 @@
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import App from './App'
+import useTheme from '../shared/hooks/useTheme'
+
+jest.mock('../shared/hooks/useTheme', () => ({
+  __esModule: true,
+  default: jest.fn()
+}))
+
+describe('App', () => {
+  it('calls useTheme on render', () => {
+    render(<App />)
+
+    expect(useTheme).toHaveBeenCalledTimes(1)
+  })
+})
 
 describe('App component', () => {
   it('show welcome message', () => {
     render(<App />)
 
-    const loadingText = screen.getByText(/Hello World/i)
-    expect(loadingText).toBeInTheDocument()
+    const desktopLayout = document.getElementsByClassName('appRoot')[0]
+    expect(desktopLayout).toBeInTheDocument()
   })
 })
