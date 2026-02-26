4716dd2a6f82f776c09dc89d6b45df2a896634ba
Noabsa <noelia.abalo@gmail.com>
2026-02-26 11:16:58 +0100

    ✅ test(app): fix App tests

--- Todoy/src/app/App.test.tsx
+++ Todoy/src/app/App.test.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import App from './App'
 import useTheme from '../shared/hooks/useTheme'
 
@@ -8,17 +8,19 @@ jest.mock('../shared/hooks/useTheme', () => ({
 }))
 
 describe('App', () => {
-  it('calls useTheme on render', () => {
+  it('calls useTheme on render', async () => {
     render(<App />)
 
+    await screen.findByRole('main')
     expect(useTheme).toHaveBeenCalledTimes(1)
   })
 })
 
 describe('App component', () => {
-  it('show welcome message', () => {
+  it('show welcome message', async () => {
     render(<App />)
 
+    await screen.findByRole('main')
     const desktopLayout = document.getElementsByClassName('appRoot')[0]
     expect(desktopLayout).toBeInTheDocument()
   })
