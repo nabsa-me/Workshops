859205e1291d226279b8b455ff4b8867723920ae
Noabsa <noelia.abalo@gmail.com>
2026-03-04 10:54:48 +0100

    🎥 lesson(ui): award winning marketing websites - accessibility

--- FEM/Award-Winning-Marketing-Websites/src/app/(experiments)/9-reduced-motion/starter/index.tsx
+++ FEM/Award-Winning-Marketing-Websites/src/app/(experiments)/9-reduced-motion/starter/index.tsx
@@ -1,10 +1,15 @@
 "use client";
 
 import { useGSAP } from "@gsap/react";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import gsap from "gsap";
 import { SplitText, ScrollTrigger } from "gsap/all";
 import { fitContent, remap } from "@/lib/math";
+import NextImage from "next/image"
+
+import heroImage from "./assets/0001.webp"
+import cameraImage from "./assets/0130.webp"
+import wheelsImage from "./assets/0300.webp"
 
 gsap.registerPlugin(SplitText);
 gsap.registerPlugin(ScrollTrigger);
@@ -12,6 +17,26 @@ gsap.registerPlugin(ScrollTrigger);
 export default function Page() {
   const containerRef = useRef<HTMLDivElement>(null);
   const progress = useRef<number>(0);
+
+  const [reducedMotion, setReducedMotion] = useState(false)
+
+  useEffect(()=>{
+    const controller = new AbortController()
+    const query = window.matchMedia("(prefers-reduced-motion: reduce)")
+
+    query.addEventListener("change", ()=>{
+      setReducedMotion(query.matches)
+    },{signal: controller.signal})
+
+
+    // eslint-disable-next-line
+    setReducedMotion(query.matches)
+
+    return () => {
+      controller.abort()
+    }
+
+  },[])
 
   useGSAP(
     () => {
@@ -22,7 +47,9 @@ export default function Page() {
         mask: "chars",
       });
 
-      gsap.from("h1 .char", {
+      const mm = gsap.matchMedia()
+      mm.add("(prefers-reduced-motion: no-preference)", ()=>{
+        gsap.from("h1 .char", {
         x: "100%",
         rotateY: "90deg",
         stagger: 0.02,
@@ -71,20 +98,25 @@ export default function Page() {
           },
           "-=0.2"
         );
+        
+      })
     },
     { scope: containerRef }
   );
 
   return (
-    <div ref={containerRef} className="h-[400vh]">
-      <ImageSequence progress={progress} />
+    <div ref={containerRef} className="motion-safe:h-[400vh]">
+      {!reducedMotion && <ImageSequence progress={progress} />}
       <div className="relative w-full overflow-clip">
-        <section className="title h-screen fixed w-full">
+        <section className="title h-screen fixed w-full motion-reduce:relative">
+          <NextImage src={heroImage} className="absolute inset-0 h-full object-cover motion-safe:hidden" alt="Perseverance rover"/>
           <h1 className="uppercase absolute text-[8vw] w-full text-center -bottom-[0.1em] leading-none right-[0.05em] tracking-widest text-transparent">
             Perseverance
           </h1>
         </section>
-        <section className="cameras fixed h-screen w-full top-0 left-0 opacity-0">
+        <section className="cameras fixed h-screen w-full top-0 left-0 motion-safe:opacity-0 motion-reduce:relative">
+          <NextImage src={cameraImage} className="absolute inset-0 h-full object-cover motion-safe:hidden" alt="Perseverance rover"/>
+
           <div className="absolute top-1/2 -translate-y-1/2 right-10 max-w-full w-md text-white">
             <h2 className="text-6xl mb-2">Cameras</h2>
             <p className="text-balance">
@@ -95,7 +127,9 @@ export default function Page() {
             </p>
           </div>
         </section>
-        <section className="wheels fixed h-screen w-full opacity-0">
+        <section className="wheels fixed h-screen w-full motion-safe:opacity-0 motion-reduce:relative">
+          <NextImage src={wheelsImage} className="absolute inset-0 h-full object-cover motion-safe:hidden" alt="Perseverance rover"/>
+
           <div className="absolute bottom-10 left-16 max-w-full w-md text-white">
             <h2 className="text-6xl mb-2">Wheels</h2>
             <p className="text-balance">
