07ce5cff4ab4888f0c39570de7c9e686475ae5b8
Noabsa <noelia.abalo@gmail.com>
2026-03-04 11:21:23 +0100

    🎥 lesson(ui): award winning marketing websites - three.js

--- FEM/Award-Winning-Marketing-Websites/src/app/(experiments)/11-animating-3d-scenes/starter.tsx
+++ FEM/Award-Winning-Marketing-Websites/src/app/(experiments)/11-animating-3d-scenes/starter.tsx
@@ -1,19 +1,54 @@
 "use client";
 
+import { useGSAP } from "@gsap/react";
 import { PerspectiveCamera, useGLTF } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import gsap from "gsap";
+import { useState } from "react";
+import { Group, PointLight } from "three";
+// import { Group } from "three/examples/jsm/libs/tween.module.js";
 
 export default function Page() {
+  const [groupRef, setGroupRef] = useState<Group | null>(null)
+  const [lightRef, setLightRef] = useState<PointLight | null>(null)
+
+  useGSAP(()=>{
+    if(!groupRef || !lightRef) return 
+
+    gsap.from(groupRef.position,{
+      y: -1,
+    })
+
+    gsap.from(groupRef.rotation,{
+      x: 0.5,
+      duration:1,
+      ease:"power2.out"
+    })
+
+    gsap.from(lightRef,{
+      intensity: 0,
+      duration:1.2,
+      ease:"power2.inOut"
+    })
+
+    gsap.to(lightRef.position,{
+      x: -4,
+      duration:1.2,
+      ease:"power2.inOut"
+    })
+
+
+  },{dependencies: [groupRef, lightRef]})
+
   return (
     <div className="absolute h-screen w-screen top-0 left-0 bg-black">
       <Canvas>
-        <group>
-          <Scene />
+        <group ref={setGroupRef}>
+          <Scene/>
         </group>
-        <PerspectiveCamera fov={28} makeDefault position={[0, 1.5, 5]} />
-        <ambientLight intensity={0.1} />
-        <pointLight position={[30, 3, 5]} intensity={20} />
-        <color args={["black"]} attach="background" />
+        <pointLight ref={setLightRef} position={[2,2,2]} intensity={20}/>
+        <PerspectiveCamera makeDefault position={[0,1.5,5]} fov={28}/>
+        {/* <OrbitControls/> */}
       </Canvas>
     </div>
   );
