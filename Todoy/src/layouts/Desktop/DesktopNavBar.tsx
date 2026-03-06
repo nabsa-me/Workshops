b92f7757adcb08868322761a2596025cacce91f0
Noabsa <noelia.abalo@gmail.com>
2026-03-06 10:41:28 +0100

    ✨ feat(app): add burger button at navbar to sidebar management

--- Todoy/src/layouts/Desktop/DesktopNavBar.tsx
+++ Todoy/src/layouts/Desktop/DesktopNavBar.tsx
@@ -6,8 +6,11 @@ const DesktopNavBar = ({ setSideBarHidden, sideBarHidden }: IDesktopNavBarProps)
   const { filter } = useTasks()
 
   return (
-    <div className='desktop-navBar' onClick={() => setSideBarHidden(sideBarHidden ? '' : 'hidden')}>
-      <IconButton icon='menu' className='burger-button' type='thin' />
+    <div className='desktop-navBar'>
+      <div onClick={() => setSideBarHidden(sideBarHidden ? '' : 'hidden')} className='burger-button-container'>
+        <IconButton icon='menu_open' className={`burger-button-open ${sideBarHidden}`} type='thin' />
+        <IconButton icon='menu' className={`burger-button ${sideBarHidden ? '' : 'hidden'}`} type='thin' />
+      </div>
       <div className='search-bar-container'>
         <Icon icon='search' className='search-bar-icon' type='thin' />
         <form className='search-bar-form' onSubmit={(event) => event.preventDefault()}>
