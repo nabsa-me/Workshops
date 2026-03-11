90d39f87de91c7401641f161c63105e1af1c582c
Noabsa <noelia.abalo@gmail.com>
2026-03-11 18:36:19 +0100

    🐛 fix(aws): delete unneeded environment variable in tasks stack

--- Todoy-Server/cdk/lib/todoyTasksService.ts
+++ Todoy-Server/cdk/lib/todoyTasksService.ts
@@ -24,8 +24,7 @@ export class TodoyTasksStack extends Stack {
       runtime: Runtime.NODEJS_22_X,
       architecture: Architecture.ARM_64,
       handler: 'handler',
-      entry: path.join(__dirname, '../../src/services/tasks/handler.ts'),
-      environment: { env: environment || 'dev' }
+      entry: path.join(__dirname, '../../src/services/tasks/handler.ts')
     })
 
     const aliasVersion = stage === 'stage' ? lambda.currentVersion.version : '$LATEST'
